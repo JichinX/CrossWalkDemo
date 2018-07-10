c45967aaae9be105024417558471bfe93fc31adb
xujichang <xujichang@outlook.com>
2018-07-10 10:44:54 +0800

    添加webview模块

--- test/src/App.js
+++ test/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { Component, Text } from "react";
 import "./App.css";
-
+import ProtoColTest from "./component/Protocol/ProtocolTest";
+import SupportTest from "./component/Support/SupportTest";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>CrossWalk测试Demo</p>
+        <ProtoColTest />
+        <SupportTest />
       </div>
     );
   }
 }
-
 export default App;
